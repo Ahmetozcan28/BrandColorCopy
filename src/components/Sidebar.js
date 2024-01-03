de7d471a20530d27ff18cdf7d5e096a347e4f5d7
Ahmetozcan28 <mrkronoss@hotmail.com>
2024-01-03 15:11:14 +0300

    BrandColor commit

---
+++ src/components/Sidebar.js
@@ -0,0 +1,33 @@
+import React from "react";
+
+export default function Sidebar() {
+  return (
+    <>
+      <aside className="sidebar">
+        <div className="logo">
+          <a>
+            Brand <b>Colors</b>
+          </a>
+        </div>
+
+        <div className="description">
+          The biggest collection of official brand color codes around. Curated
+          by @brandcolors and friends.
+        </div>
+        <nav className="menu">
+          <ul>
+            <li>
+              <a>About BrandColors</a>
+            </li>
+          </ul>
+        </nav>
+      </aside>
+    </>
+  );
+}
+
+// Suggest a Brand
+// About BrandColors
+
+// BrandColors + DesignBombs
+// Learn how to make a website - we have put together an in-depth guide that will help you build your first website with WordPress.
